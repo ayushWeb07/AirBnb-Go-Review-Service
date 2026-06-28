a0454f138262fa7ff557b8b1a75adc9559d6c845
Ayush Bommana <bommanaayush07@gmail.com>
2026-06-28 21:12:35 +0530

    feat: Integrate with other microservices

--- internal/config/server_config.go
+++ internal/config/server_config.go
@@ -8,13 +8,16 @@ import (
 )
 
 type ServerConfig struct {
-	Addr              string        `validate:"required"`
-	ReadTimeout       time.Duration `validate:"required"`
-	WriteTimeout      time.Duration `validate:"required"`
-	IdleTimeout       time.Duration `validate:"required"`
-	AppEnv            string        `validate:"required"`
-	JwtSecretKey      string        `validate:"required"`
-	RequestsPerMinute int           `validate:"required"`
+	Addr                  string        `validate:"required"`
+	ReadTimeout           time.Duration `validate:"required"`
+	WriteTimeout          time.Duration `validate:"required"`
+	IdleTimeout           time.Duration `validate:"required"`
+	AppEnv                string        `validate:"required"`
+	JwtSecretKey          string        `validate:"required"`
+	RequestsPerMinute     int           `validate:"required"`
+	HotelServiceBaseUrl   string        `validate:"required"`
+	BookingServiceBaseUrl string        `validate:"required"`
+	ApiGatewayBaseUrl     string        `validate:"required"`
 }
 
 func LoadServerConfig() (*ServerConfig, error) {
@@ -27,13 +30,16 @@ func LoadServerConfig() (*ServerConfig, error) {
 
 	// load the envs & create the config instance
 	cfg := &ServerConfig{
-		Addr:              LoadSingleEnvVar("ADDR", ":3030"),
-		ReadTimeout:       time.Duration(LoadSingleEnvVar("READ_TIMEOUT", 15)) * time.Second,
-		WriteTimeout:      time.Duration(LoadSingleEnvVar("WRITE_TIMEOUT", 15)) * time.Second,
-		IdleTimeout:       time.Duration(LoadSingleEnvVar("IDLE_TIMEOUT", 180)) * time.Second,
-		AppEnv:            LoadSingleEnvVar("APP_ENV", "development"),
-		JwtSecretKey:      LoadSingleEnvVar("JWT_SECRET_KEY", ""),
-		RequestsPerMinute: LoadSingleEnvVar("REQUESTS_PER_MINUTE", 100),
+		Addr:                  LoadSingleEnvVar("ADDR", ":3030"),
+		ReadTimeout:           time.Duration(LoadSingleEnvVar("READ_TIMEOUT", 15)) * time.Second,
+		WriteTimeout:          time.Duration(LoadSingleEnvVar("WRITE_TIMEOUT", 15)) * time.Second,
+		IdleTimeout:           time.Duration(LoadSingleEnvVar("IDLE_TIMEOUT", 180)) * time.Second,
+		AppEnv:                LoadSingleEnvVar("APP_ENV", "development"),
+		JwtSecretKey:          LoadSingleEnvVar("JWT_SECRET_KEY", ""),
+		RequestsPerMinute:     LoadSingleEnvVar("REQUESTS_PER_MINUTE", 100),
+		HotelServiceBaseUrl:   LoadSingleEnvVar("HOTEL_SERVICE_BASE_URL", ""),
+		BookingServiceBaseUrl: LoadSingleEnvVar("BOOKING_SERVICE_BASE_URL", ""),
+		ApiGatewayBaseUrl:     LoadSingleEnvVar("API_GATEWAY_BASE_URL", ""),
 	}
 
 	return cfg, nil
--- internal/services/review_service.go
+++ internal/services/review_service.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/ayushWeb07/AirBnb-Go-Review-Service/internal/config"
 	"github.com/ayushWeb07/AirBnb-Go-Review-Service/internal/database/models"
 	"github.com/ayushWeb07/AirBnb-Go-Review-Service/internal/dtos"
@@ -26,6 +29,81 @@ type ReviewService struct {
 func (reviewService *ReviewService) CreateReview(reviewPayload *dtos.CreateReviewDTO) *utils.AppError {
 	reviewService.logger.Info("Create review service called...")
 
+	// check if the user exists
+	apiGatewayUrl := reviewService.serverConfig.ApiGatewayBaseUrl + "/users/" + strconv.Itoa(reviewPayload.UserID)
+
+	resp, err := http.Get(apiGatewayUrl)
+	if err != nil {
+		reviewService.logger.Error("Failed to make request to api gateway: " + err.Error())
+		return utils.InternalServerError("Failed to make request to api gateway: " + err.Error())
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+
+		reviewService.logger.Error("Such user not found",
+			zap.Int("user_id", reviewPayload.UserID))
+
+		return utils.NotFound("User with such id not found")
+
+	} else if resp.StatusCode != http.StatusOK {
+
+		reviewService.logger.Error("Something went wrong while checking if the user exists",
+			zap.Int("user_id", reviewPayload.UserID))
+
+		return utils.InternalServerError("Something went wrong while checking if the user exists")
+
+	}
+
+	// check if the hotel exists
+	hotelServiceUrl := reviewService.serverConfig.HotelServiceBaseUrl + "/hotels/" + strconv.Itoa(reviewPayload.HotelID)
+
+	resp, err = http.Get(hotelServiceUrl)
+	if err != nil {
+		reviewService.logger.Error("Failed to make request to hotel service: " + err.Error())
+		return utils.InternalServerError("Failed to make request to hotel service: " + err.Error())
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+
+		reviewService.logger.Error("Such hotel not found",
+			zap.Int("hotel_id", reviewPayload.HotelID))
+
+		return utils.NotFound("Hotel with such id not found")
+
+	} else if resp.StatusCode != http.StatusOK {
+
+		reviewService.logger.Error("Something went wrong while checking if the hotel exists",
+			zap.Int("hotel_id", reviewPayload.HotelID))
+
+		return utils.InternalServerError("Something went wrong while checking if the hotel exists")
+
+	}
+
+	// check if the booking exists
+	bookingServiceUrl := reviewService.serverConfig.BookingServiceBaseUrl + "/bookings/" + strconv.Itoa(reviewPayload.BookingID)
+
+	resp, err = http.Get(bookingServiceUrl)
+	if err != nil {
+		reviewService.logger.Error("Failed to make request to booking service: " + err.Error())
+		return utils.InternalServerError("Failed to make request to booking service: " + err.Error())
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+
+		reviewService.logger.Error("Such booking not found",
+			zap.Int("booking_id", reviewPayload.BookingID))
+
+		return utils.NotFound("Booking with such id not found")
+
+	} else if resp.StatusCode != http.StatusOK {
+
+		reviewService.logger.Error("Something went wrong while checking if the booking exists",
+			zap.Int("booking_id", reviewPayload.BookingID))
+
+		return utils.InternalServerError("Something went wrong while checking if the booking exists")
+
+	}
+
 	// call the create review repository
 	repositoryErr := reviewService.ReviewRepository.CreateReview(reviewPayload)
 	return repositoryErr
@@ -33,6 +111,31 @@ func (reviewService *ReviewService) CreateReview(reviewPayload *dtos.CreateRevie
 
 func (reviewService *ReviewService) GetAllReviewsByHotelId(reviewParams *dtos.GetAllReviewsByHotelIdDTO) ([]*models.ReviewModel, *utils.AppError) {
 	reviewService.logger.Info("Get all reviews service called...")
+
+	// check if the hotel exists
+	url := reviewService.serverConfig.HotelServiceBaseUrl + "/hotels/" + strconv.Itoa(reviewParams.HotelID)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		reviewService.logger.Error("Failed to make request to hotel service: " + err.Error())
+		return nil, utils.InternalServerError("Failed to make request to hotel service: " + err.Error())
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+
+		reviewService.logger.Error("Such hotel not found",
+			zap.Int("hotel_id", reviewParams.HotelID))
+
+		return nil, utils.NotFound("Hotel with such id not found")
+
+	} else if resp.StatusCode != http.StatusOK {
+
+		reviewService.logger.Error("Something went wrong while checking if the hotel exists",
+			zap.Int("hotel_id", reviewParams.HotelID))
+
+		return nil, utils.InternalServerError("Something went wrong while checking if the hotel exists")
+
+	}
 
 	// call the fetch all reviews repository
 	reviewModels, repositoryErr := reviewService.ReviewRepository.GetAllReviewsByHotelId(reviewParams)
